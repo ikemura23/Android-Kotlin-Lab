50c3f3cffc9f22366379aec45cc6c24792f35f6c
ikemura <k_ikemura@yumemi.co.jp>
2018-08-03 12:52:40 +0900

    アニメーションたくさんついか

--- app/src/main/java/com/ikemura/android_kotlin_lab/MainActivity.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.ikemura.android_kotlin_lab
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.star
+import kotlinx.android.synthetic.main.activity_main.star_layout
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupClickEvent()
+    }
+
+    private fun setupClickEvent() {
+        star_layout.setOnClickListener {
+            star.playAnimation()
+        }
     }
 }
