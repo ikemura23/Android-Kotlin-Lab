fa72e59082f9805f65413e092e5e7a818a863b3b
ikemura <k_ikemura@yumemi.co.jp>
2018-09-07 16:29:39 +0900

    add fragment

---
+++ app/src/main/java/com/ikemura/android_kotlin_lab/BlankFragment.kt
@@ -0,0 +1,22 @@
+package com.ikemura.android_kotlin_lab
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class BlankFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+
+}
--- app/src/main/java/com/ikemura/android_kotlin_lab/MainActivity.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.ikemura.android_kotlin_lab
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.main_container, BlankFragment())
+                .commit()
     }
 }
