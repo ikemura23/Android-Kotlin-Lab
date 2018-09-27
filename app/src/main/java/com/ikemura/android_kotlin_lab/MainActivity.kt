b07cc89f9c09fcd31f3150196e1629fc61f024e6
ikemura <k_ikemura@yumemi.co.jp>
2018-09-27 15:30:47 +0900

    広告IDを取得する

--- app/src/main/java/com/ikemura/android_kotlin_lab/MainActivity.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.ikemura.android_kotlin_lab
 
-import android.bluetooth.le.AdvertiseSettings
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.identifier.AdvertisingIdClient
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread(Runnable {
+            Log.d("MainActivity", getAdvertiseId(this))
+        }).start()
     }
 }
+
+fun getAdvertiseId(
+        context: Context) = AdvertisingIdClient.getAdvertisingIdInfo(context).id.toString()
+
