726710ef1c543c40b740878e64c9b098c42c51fd
ikemura <k_ikemura@yumemi.co.jp>
2018-10-04 23:30:53 +0900

    work manager

--- app/src/main/java/com/ikemura/android_kotlin_lab/MainActivity.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.ikemura.android_kotlin_lab
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import kotlinx.android.synthetic.main.activity_main.button1
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            startWorkManager()
+        }
+    }
+
+    private fun startWorkManager() {
+        val testWorker = OneTimeWorkRequestBuilder<TestWorker>().build()
+        WorkManager.getInstance().enqueue(testWorker)
+    }
+}
+
+class TestWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+    override fun doWork(): Result {
+        Log.d("MainActivity", "WorkManager Test")
+        return Result.SUCCESS
     }
 }
