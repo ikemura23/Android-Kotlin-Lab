ea4f88157f92136be1bdb1527a3e6f4837f8a93d
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2018-10-23 20:01:19 +0900

    add ImageDecoder

--- app/src/main/java/com/ikemura/android_kotlin_lab/MainActivity.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.ikemura.android_kotlin_lab
 
+import android.graphics.ImageDecoder
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.image
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupImageDecoder()
+    }
+
+    /**
+     * @see https://developer.android.com/about/versions/pie/android-9.0?hl=ja
+     * @see https://developer.android.com/reference/android/graphics/ImageDecoder?hl=ja
+     */
+    private fun setupImageDecoder() {
+        val src = ImageDecoder.createSource(resources.assets, "droid.jpeg")
+        val drawable = ImageDecoder.decodeDrawable(src)
+
+        image.setImageDrawable(drawable)
     }
 }
