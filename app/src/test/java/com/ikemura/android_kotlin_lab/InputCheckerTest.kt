968e69fa27fe5b0da18031cd9845092e374f7ae7
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2018-11-15 10:55:50 +0900

    空のテストケースクラス作成

--- app/src/test/java/com/ikemura/android_kotlin_lab/InputCheckerTest.kt
+++ app/src/test/java/com/ikemura/android_kotlin_lab/InputCheckerTest.kt
@@ -1,8 +1,6 @@
 package com.ikemura.android_kotlin_lab
 
-import org.hamcrest.CoreMatchers.`is`
 import org.junit.After
-import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
 
@@ -16,13 +14,8 @@ class InputCheckerTest {
     fun tearDown() {
     }
 
-    @Test
+    @Test // JUnit4ではテストケースとして実行したいメソッドをorg.junit.Test アノテーションで修飾する
     fun isValid() {
-//        assertThat(1 + 1, `is`(2))
-//        assertThat(100, greaterThan(50))
-//        assertThat(listOf("for", "bar", "baz"), hasItem("baz"))
-        val target = InputChecker()
-        val actual = target.isValid("test")
-        assertThat(actual, `is`(true))
+        //ここがテストケースとして実行される
     }
 }
