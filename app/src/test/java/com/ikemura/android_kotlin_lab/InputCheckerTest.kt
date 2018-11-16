522cb90484841b8f0178919b5b4bfe0df2034de8
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2018-11-16 18:37:39 +0900

    2.2.5 はじめてのユニットテスト

--- app/src/test/java/com/ikemura/android_kotlin_lab/InputCheckerTest.kt
+++ app/src/test/java/com/ikemura/android_kotlin_lab/InputCheckerTest.kt
@@ -1,6 +1,8 @@
 package com.ikemura.android_kotlin_lab
 
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.After
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
 
@@ -16,6 +18,8 @@ class InputCheckerTest {
 
     @Test // JUnit4ではテストケースとして実行したいメソッドをorg.junit.Test アノテーションで修飾する
     fun isValid() {
-        //ここがテストケースとして実行される
+        val target = InputChecker() //InputCheckerのインスタンスを生成
+        val actual = target.isValid("foo")  //fooという文字列を渡して、実測値をactualに格納
+        assertThat(actual, `is`(true))  //assertThat()で実測値と期待値が一致することを検証
     }
 }
