0accda630c4ebbb34848db3a688a9b70d0dde9c2
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2019-01-25 23:42:32 +0900

    rssparserじゃxmlのRssが読み込めないためandroid-rssライブラリを使ってみた

--- app/src/main/java/com/ikemura/android_kotlin_lab/rss/RssFragment.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/rss/RssFragment.kt
@@ -2,15 +2,19 @@ package com.ikemura.android_kotlin_lab.rss
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.ikemura.android_kotlin_lab.R
 import com.ikemura.android_kotlin_lab.rss.dummy.DummyContent
 import com.ikemura.android_kotlin_lab.rss.dummy.DummyContent.DummyItem
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.mcsoxford.rss.RSSReader
 
 class RssFragment : Fragment() {
 
@@ -18,7 +22,7 @@ class RssFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_rss_list, container, false)
+        val view = inflater.inflate(com.ikemura.android_kotlin_lab.R.layout.fragment_rss_list, container, false)
 
         // Set the adapter
         if (view is RecyclerView) {
@@ -28,6 +32,27 @@ class RssFragment : Fragment() {
             }
         }
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fetchRss()
+    }
+
+    private fun fetchRss() {
+        val url = "http://www.aneikankou.co.jp/news/index.xml"
+        GlobalScope.launch {
+
+            try {
+                val reader = RSSReader()
+                val uri = "http://www.aneikankou.co.jp/news/index.xml"
+                val feed = reader.load(uri)
+                feed.items.forEach { Log.d("RssFragment",it.toString()) }
+            } catch (e: Exception) {
+                Log.d("RssFragment", e.message)
+                e.printStackTrace()
+            }
+        }
     }
 
     override fun onAttach(context: Context) {
