2475b8bd0928a3d6f823389bf3c12e5db6e8652c
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2019-02-07 11:45:25 +0900

    cherry pic

--- app/src/main/java/com/ikemura/android_kotlin_lab/main/MainActivity.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ikemura.android_kotlin_lab.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import com.ikemura.android_kotlin_lab.R
 import com.ikemura.android_kotlin_lab.list.ItemFragment
 
@@ -14,9 +15,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupFragment() {
-        supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.container, ItemFragment.newInstance(50))
-                .commitNow()
+        supportFragmentManager.commit(false) {
+            replace(R.id.container, ItemFragment.newInstance(50))
+        }
     }
 }
