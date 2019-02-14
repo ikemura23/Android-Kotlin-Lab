d5dd34db3da29161646a694b65e5c8210d9e05b2
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2019-02-14 18:20:10 +0900

    非同期ブロックが例外をスローする可能性がある場合は、try / catchブロックでラップすることに頼らない

--- app/src/main/java/com/ikemura/android_kotlin_lab/main/MainActivity.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/main/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.ikemura.android_kotlin_lab.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.ikemura.android_kotlin_lab.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeoutException
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+    private val job = Job()
+    private val scope = CoroutineScope(Dispatchers.Default + job)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +29,21 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.commit(false) {
             replace(R.id.container, MainFragment.newInstance())
         }
+    }
+
+    // タイムアウトエラーの可能性がある処理
+    fun doWork(): Deferred<String> = scope.async { throw TimeoutException() }
+
+    fun loadData() = scope.launch {
+        try {
+            doWork().await()
+        } catch (e: Exception) {
+            Log.d(TAG, "ここに到達しない")
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadData()
     }
 }
