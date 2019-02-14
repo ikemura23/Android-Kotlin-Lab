72a1d4bb09e0106d661f94859d90eccd25a4563d
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2019-02-15 00:57:36 +0900

    CoroutineExceptionHandlerを個別に指定

--- app/src/main/java/com/ikemura/android_kotlin_lab/main/MainActivity.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/main/MainActivity.kt
@@ -5,18 +5,20 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.ikemura.android_kotlin_lab.R
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import java.util.concurrent.TimeoutException
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
+    private val exceptionHandler: CoroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Log.e(TAG, "例外キャッチ $throwable")
+    }
     private val job = SupervisorJob()
     private val scope = CoroutineScope(Dispatchers.Default + job)
 
@@ -33,18 +35,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     // エラーの可能性がある処理
-    suspend fun doWork(): Deferred<String> = coroutineScope {
-        async { throw TimeoutException() }
-    }
+    fun doWork(): Deferred<String> = throw TimeoutException()
 
     fun loadData() {
-        scope.launch {
-            try {
-                doWork()
-                Log.d(TAG, "ここは実行されない")
-            } catch (e: Exception) {
-                Log.d(TAG, "キャッチされる")
-            }
+        scope.launch(exceptionHandler) {
+            doWork()
+            Log.d(TAG, "到達しない")
         }
     }
 
