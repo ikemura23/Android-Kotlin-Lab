1feecf8caf7b2c13340fc86c46cdedf6b517d8e8
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2019-03-19 16:44:33 +0900

    Create WebViewFragment

---
+++ app/src/main/java/com/ikemura/android_kotlin_lab/webview/WebViewFragment.kt
@@ -0,0 +1,38 @@
+package com.ikemura.android_kotlin_lab.webview
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.ikemura.android_kotlin_lab.R
+import kotlinx.android.synthetic.main.web_view_fragment.webView
+
+class WebViewFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.web_view_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupWebView()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView() {
+        val url = "https://www.dailymotion.com/video/x5x7cgf"
+        webView.apply {
+            settings.javaScriptEnabled = true
+            webChromeClient = WebChromeClient()
+            webViewClient = WebViewClient()
+            settings.cacheMode = WebSettings.LOAD_NO_CACHE
+        }.loadUrl(url)
+    }
+}
