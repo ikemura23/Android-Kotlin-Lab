8e865c506a17cfc9a35b8aa76fa5ea5fce893837
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2019-04-19 14:33:51 +0900

    MainActivityにfirebaseAnalitics追加

--- app/src/main/java/com/ikemura/android_kotlin_lab/main/MainActivity.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/main/MainActivity.kt
@@ -3,13 +3,17 @@ package com.ikemura.android_kotlin_lab.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.ikemura.android_kotlin_lab.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setupFragment()
     }
 
