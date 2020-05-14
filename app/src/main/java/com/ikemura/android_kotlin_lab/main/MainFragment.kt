182a76a3386e278a99fb2396b2e76356fc3ec86d
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-05-14 18:10:49 +0900

    buttonクリックで viewModel.load を開始させつつ、Toast表示させる

--- app/src/main/java/com/ikemura/android_kotlin_lab/main/MainFragment.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/main/MainFragment.kt
@@ -1,10 +1,12 @@
 package com.ikemura.android_kotlin_lab.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -27,6 +29,9 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupViewModel()
+        binding.button.setOnClickListener {
+            viewModel.load()
+        }
     }
 
     override fun onResume() {
@@ -34,25 +39,32 @@ class MainFragment : Fragment() {
         viewModel.load()
     }
 
+    @SuppressLint("SetTextI18n")
     private fun setupViewModel() {
         // 状態の管理
         viewModel.state.observe(viewLifecycleOwner, Observer<ScreenState> { state ->
             when (state) {
                 is ScreenState.Loading -> {
                     //ローディング処理
-                    Log.d("MainFragment", "Loading")
+                    setMessageText("Loading")
                 }
                 is ScreenState.Data -> {
                     //データ取得
-                    Log.d("MainFragment", state.someData.toString())
-                    binding.message.text = "${state.someData.id} ${state.someData.label}"
+                    setMessageText("Success: ${state.someData}")
+                    Toast.makeText(context, "Success: ${state.someData}", Toast.LENGTH_SHORT).show()
                 }
                 is ScreenState.Error -> {
                     //エラー処理
                     Log.d("MainFragment", "Error")
+                    setMessageText("Error")
                 }
             }
         })
+    }
+
+    private fun setMessageText(state: String) {
+        Log.d("MainFragment", state)
+        binding.message.text = state
     }
 
     companion object {
