2b9307497498d662d015c1ed3894e00140114935
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-05-27 01:13:22 +0900

    transformアニメーション完成

--- app/src/main/java/com/ikemura/android_kotlin_lab/main/MainFragment.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/main/MainFragment.kt
@@ -1,11 +1,15 @@
 package com.ikemura.android_kotlin_lab.main
 
+import android.graphics.Color
 import android.os.Bundle
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.google.android.material.transition.MaterialArcMotion
+import com.google.android.material.transition.MaterialContainerTransform
 import com.ikemura.android_kotlin_lab.R
 import com.ikemura.android_kotlin_lab.databinding.MainFragmentBinding
 
@@ -22,6 +26,33 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // FAB -> Card
+        binding.fab.setOnClickListener { fab ->
+            val transform = MaterialContainerTransform(requireContext()).apply {
+                startView = fab
+                endView = binding.card
+                pathMotion = MaterialArcMotion()
+                duration = 500
+                scrimColor = Color.TRANSPARENT
+            }
+            TransitionManager.beginDelayedTransition(binding.container, transform)
+            fab.visibility = View.GONE
+            binding.card.visibility = View.VISIBLE
+        }
+
+        // Card -> Fab
+        binding.card.setOnClickListener { card ->
+            val transform = MaterialContainerTransform(requireContext()).apply {
+                startView = card
+                endView = binding.fab
+                pathMotion = MaterialArcMotion()
+                duration = 500
+                scrimColor = Color.TRANSPARENT
+            }
+            TransitionManager.beginDelayedTransition(binding.container, transform)
+            binding.fab.visibility = View.VISIBLE
+            card.visibility = View.GONE
+        }
     }
 
     companion object {
