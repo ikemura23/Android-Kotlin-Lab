313bf9a4227b368d85510c0a0effc6a020d64844
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-06-18 16:27:33 +0900

    CameraProvider をリクエストする

--- app/src/main/java/com/ikemura/android_kotlin_lab/camera/CameraFragment.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/camera/CameraFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.fragment.app.Fragment
+import com.google.common.util.concurrent.ListenableFuture
 import com.ikemura.android_kotlin_lab.databinding.FragmentCameraBinding
 
 class CameraFragment : Fragment() {
     private lateinit var binding: FragmentCameraBinding
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +23,11 @@ class CameraFragment : Fragment() {
     ): View? {
         binding = FragmentCameraBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
     }
 
     companion object {
