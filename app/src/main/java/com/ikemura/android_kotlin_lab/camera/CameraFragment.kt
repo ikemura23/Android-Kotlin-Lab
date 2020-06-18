6bf5e6e338f5fb697f0a80b054d40e06a95062cd
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-06-18 22:07:28 +0900

    画像を解析するするため ImageAnalysis を追加

--- app/src/main/java/com/ikemura/android_kotlin_lab/camera/CameraFragment.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/camera/CameraFragment.kt
@@ -1,11 +1,13 @@
 package com.ikemura.android_kotlin_lab.camera
 
 import android.os.Bundle
+import android.util.Size
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.camera.core.Camera
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
@@ -14,11 +16,13 @@ import androidx.lifecycle.LifecycleOwner
 import com.google.common.util.concurrent.ListenableFuture
 import com.ikemura.android_kotlin_lab.databinding.FragmentCameraBinding
 import kotlinx.android.synthetic.main.fragment_camera.viewFinder
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 class CameraFragment : Fragment() {
     private lateinit var binding: FragmentCameraBinding
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
-
+    private lateinit var executor: Executor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -34,6 +38,7 @@ class CameraFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        executor = Executors.newSingleThreadExecutor()
         cameraProviderFuture.addListener(Runnable {
             val cameraProvider = cameraProviderFuture.get()
             bindPreview(cameraProvider)
@@ -48,7 +53,17 @@ class CameraFragment : Fragment() {
             .requireLensFacing(CameraSelector.LENS_FACING_BACK)
             .build()
         cameraProvider.unbindAll()
-        val camera: Camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setTargetResolution(Size(1280, 720))
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+        imageAnalysis.setAnalyzer(executor, ImageAnalysis.Analyzer { imageProxy ->
+            val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+            // insert your code here
+        })
+
+        val camera: Camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, imageAnalysis, preview)
         preview.setSurfaceProvider(viewFinder.createSurfaceProvider())
     }
 
