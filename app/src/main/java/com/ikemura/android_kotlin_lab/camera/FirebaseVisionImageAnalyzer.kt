499e88019299b631fb4c5ec7ce5b5b65d02ec239
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-06-23 20:54:32 +0900

    日本語公式ドキュメントが古いようで英語ドキュメントを見ながらFirebaseMLを実装

--- app/src/main/java/com/ikemura/android_kotlin_lab/camera/FirebaseVisionImageAnalyzer.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/camera/FirebaseVisionImageAnalyzer.kt
@@ -2,9 +2,16 @@ package com.ikemura.android_kotlin_lab.camera
 
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
 
 class FirebaseVisionImageAnalyzer : ImageAnalysis.Analyzer {
 
-    override fun analyze(image: ImageProxy) {
+    @androidx.camera.core.ExperimentalGetImage
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+            // TODO:ここに ML Kit Vision APIのコードをかく
+        }
     }
 }
