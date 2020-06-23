14e72ce993d576326ad0ddbbd60e5ad5f2605f75
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-06-23 20:46:56 +0900

    FirebaseVisionImageAnalyzerクラスを作成

---
+++ app/src/main/java/com/ikemura/android_kotlin_lab/camera/FirebaseVisionImageAnalyzer.kt
@@ -0,0 +1,10 @@
+package com.ikemura.android_kotlin_lab.camera
+
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+
+class FirebaseVisionImageAnalyzer : ImageAnalysis.Analyzer {
+
+    override fun analyze(image: ImageProxy) {
+    }
+}
