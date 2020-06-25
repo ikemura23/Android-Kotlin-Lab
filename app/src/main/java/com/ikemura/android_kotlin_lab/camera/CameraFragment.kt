1bea15ddf81ac3bd2fd1f3e0deaa789183bae7cb
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-06-25 12:13:36 +0900

    リファクタリング: 不使用コードを削除した

--- app/src/main/java/com/ikemura/android_kotlin_lab/camera/CameraFragment.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/camera/CameraFragment.kt
@@ -1,7 +1,6 @@
 package com.ikemura.android_kotlin_lab.camera
 
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import android.util.Size
 import android.view.LayoutInflater
