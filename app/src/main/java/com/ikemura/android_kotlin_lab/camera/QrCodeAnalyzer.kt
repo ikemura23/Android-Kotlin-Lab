100245bd96793f6c15b896e9eb2c15074b3c9d16
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-06-30 11:02:57 +0900

    QRコード認識できた

--- app/src/main/java/com/ikemura/android_kotlin_lab/camera/QrCodeAnalyzer.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/camera/QrCodeAnalyzer.kt
@@ -6,19 +6,19 @@ import android.os.Build
 import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
 import com.google.zxing.BarcodeFormat
-import com.google.zxing.BinaryBitmap
 import com.google.zxing.DecodeHintType
 import com.google.zxing.MultiFormatReader
-import com.google.zxing.PlanarYUVLuminanceSource
 import com.google.zxing.Result
-import com.google.zxing.common.HybridBinarizer
 
 class QrCodeAnalyzer(
     private val onQrCodeDetected: (qrCode: Result) -> Unit
 ) : ImageAnalysis.Analyzer {
 
     private val yuvFormats = mutableListOf(ImageFormat.YUV_420_888)
+    val scanner = BarcodeScanning.getClient()
 
     init {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -27,39 +27,64 @@ class QrCodeAnalyzer(
     }
 
     @SuppressLint("UnsafeExperimentalUsageError")
-    override fun analyze(image: ImageProxy) {
-        if (image.format !in yuvFormats) {
-            Log.e("QRCodeAnalyzer", "Expected YUV, now = ${image.format}")
+    override fun analyze(imageProxy: ImageProxy) {
+        if (imageProxy.format !in yuvFormats) {
+            Log.e("QRCodeAnalyzer", "Expected YUV, now = ${imageProxy.format}")
             return
         }
+        val mediaImage = imageProxy.image
+        mediaImage ?: return
+        val inputImage: InputImage = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+        // inputImage.byteBuffer ?: return
+        //
+        // val image = InputImage.fromByteBuffer(
+        //     inputImage.byteBuffer!!,
+        //     /* image width */ 480,
+        //     /* image height */ 360,
+        //     imageProxy.imageInfo.rotationDegrees,
+        //     InputImage.IMAGE_FORMAT_NV21 // or IMAGE_FORMAT_YV12
+        // )
 
-        val buffer = image.planes[0].buffer
-        buffer.rewind()
-        val byteArray = ByteArray(buffer.remaining())
-        buffer.get(byteArray) // これがないとdecodeエラーになる
-
-        val source = PlanarYUVLuminanceSource(
-            byteArray,
-            image.width,
-            image.height,
-            0,
-            0,
-            image.width,
-            image.height,
-            false
-        )
-        val binarizer = HybridBinarizer(source)
-        val binaryBitmap = BinaryBitmap(binarizer)
-        try {
-            val result = reader.decode(binaryBitmap)
-            onQrCodeDetected(result)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.e("QRCodeAnalyzer", "QRを認識できない")
-        } finally {
-            // closeしないと追加の画像の生成がブロックされる（analyzeメソッドが呼ばれない）
-            image.close()
+        scanner.process(inputImage).addOnSuccessListener {
+            if(it.isNotEmpty()) {
+                Log.d("QrCodeAnalyzer", it.first().displayValue)
+            }
+            // onQrCodeDetected(it.first())
+            // 成功
+        }.addOnFailureListener {
+            // 失敗
+        }.addOnCompleteListener {
+            imageProxy.close()
         }
+
+        // val buffer = image.planes[0].buffer
+        // buffer.rewind()
+        // val byteArray = ByteArray(buffer.remaining())
+        // buffer.get(byteArray) // これがないとdecodeエラーになる
+
+        //
+        // val source = PlanarYUVLuminanceSource(
+        //     byteArray,
+        //     image.width,
+        //     image.height,
+        //     0,
+        //     0,
+        //     image.width,
+        //     image.height,
+        //     false
+        // )
+        // val binarizer = HybridBinarizer(source)
+        // val binaryBitmap = BinaryBitmap(binarizer)
+        // try {
+        //     val result = reader.decode(binaryBitmap)
+        //     onQrCodeDetected(result)
+        // } catch (e: Exception) {
+        //     e.printStackTrace()
+        //     Log.e("QRCodeAnalyzer", "QRを認識できない")
+        // } finally {
+        //     // closeしないと追加の画像の生成がブロックされる（analyzeメソッドが呼ばれない）
+        //     image.close()
+        // }
     }
 
     private val reader = MultiFormatReader().apply {
