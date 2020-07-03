0d6ec0b9ee8537b98f002ee90291b381ada874cf
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-07-03 14:48:31 +0900

    パーミッションリクエストする実装

--- app/src/main/java/com/ikemura/android_kotlin_lab/main/MainFragment.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/main/MainFragment.kt
@@ -1,7 +1,7 @@
 package com.ikemura.android_kotlin_lab.main
 
+import android.Manifest
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -23,6 +23,14 @@ class MainFragment : Fragment() {
     private lateinit var binding: MainFragmentBinding
     private val viewModel: MainViewModel by viewModel()
 
+    private val askMultiplePermissions: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { map ->
+            // リクエストだけして許可/拒否の結果は無視する
+            for (entry in map.entries) {
+                println("askMultiplePermissions: \"${entry.key} = ${entry.value}\"")
+            }
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,13 +46,21 @@ class MainFragment : Fragment() {
             // viewModel.load()
             navigateToSecondActivity()
         }
+        requestPermission()
+    }
+
+    private fun requestPermission() {
+        val permissions = arrayOf(
+            Manifest.permission.CAMERA
+        )
+        askMultiplePermissions.launch(permissions)
     }
 
     private fun navigateToSecondActivity() {
         val launcher: ActivityResultLauncher<Intent> =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
                 // if (result.resultCode == Activity.RESULT_OK) {
-                    Log.d("MainFragment", "result code :${result.resultCode}")
+                Log.d("MainFragment", "result code :${result.resultCode}")
                 // }
             }
         launcher.launch(Intent(context, SecondActivity::class.java))
