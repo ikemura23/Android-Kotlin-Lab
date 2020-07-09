016e219573a7ab64d3bc2b4f51530de60620f33d
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-07-09 09:56:40 +0900

    カメラプレビューできるようにした

--- app/src/main/java/com/ikemura/android_kotlin_lab/main/MainActivity.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ikemura.android_kotlin_lab.main
 
+import android.Manifest
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.ikemura.android_kotlin_lab.R
@@ -8,9 +10,18 @@ import com.ikemura.android_kotlin_lab.camera.CameraFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private val askMultiplePermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { map ->
+            // リクエストだけして許可/拒否の結果は無視する
+            for (entry in map.entries) {
+                println("askMultiplePermissions: \"${entry.key} = ${entry.value}\"")
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestPermissions()
         setupFragment()
     }
 
@@ -18,5 +29,15 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.commit(false) {
             replace(R.id.container, CameraFragment.newInstance())
         }
+    }
+
+    /**
+     * 権限リクエスト
+     */
+    private fun requestPermissions() {
+        val permissions = arrayOf(
+            Manifest.permission.CAMERA
+        )
+        askMultiplePermissions.launch(permissions)
     }
 }
