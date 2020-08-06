94624628da213250318bce0c610d2dfa28ad666c
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-08-06 23:09:12 +0900

    observeEventWithFilterメソッドを追加

--- app/src/main/java/com/ikemura/android_kotlin_lab/common/extention/LiveDataExt.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/common/extention/LiveDataExt.kt
@@ -18,3 +18,18 @@ fun <T> LiveData<Event<T>>.observeEvent(lifecycleOwner: LifecycleOwner, block: (
         ev?.getContentIfNotHandled()?.let(block)
     })
 }
+typealias ObserveEventFilterPredicate<T> = (T) -> Boolean
+
+fun <T> LiveData<Event<T>>.observeEventWithFilter(
+    lifecycleOwner: LifecycleOwner,
+    filter: ObserveEventFilterPredicate<T>,
+    block: (T) -> Unit
+) {
+    observe(lifecycleOwner, Observer { ev: Event<T>? ->
+        val shouldRunBlock = ev?.peekContent()?.let(filter)
+
+        if (shouldRunBlock == true) {
+            ev.getContentIfNotHandled()?.let(block)
+        }
+    })
+}
