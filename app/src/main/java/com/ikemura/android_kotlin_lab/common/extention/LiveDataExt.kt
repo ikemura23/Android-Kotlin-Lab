7d30a282692ad91b2b48d4dba22ee18aee669fdc
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-08-06 23:00:57 +0900

    observeEventメソッドを作成した

--- app/src/main/java/com/ikemura/android_kotlin_lab/common/extention/LiveDataExt.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/common/extention/LiveDataExt.kt
@@ -3,11 +3,18 @@ package com.ikemura.android_kotlin_lab.common.extention
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import com.ikemura.android_kotlin_lab.common.Event
 
 fun <T> LiveData<T>.observeNonNull(lifecycleOwner: LifecycleOwner, block: (T) -> Unit) {
     observe(lifecycleOwner, Observer {
         if (it != null) {
             block(it)
         }
+    })
+}
+
+fun <T> LiveData<Event<T>>.observeEvent(lifecycleOwner: LifecycleOwner, block: (T) -> Unit) {
+    observe(lifecycleOwner, Observer { ev ->
+        ev?.getContentIfNotHandled()?.let(block)
     })
 }
