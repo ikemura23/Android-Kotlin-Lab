616d7ab075ced1972c7ba67ff8e5b9190a8ffdcf
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-08-06 18:32:58 +0900

    UserBindingHelperクラスを作成

--- app/src/main/java/com/ikemura/android_kotlin_lab/FragmentExt.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/FragmentExt.kt
@@ -1,6 +1,10 @@
 package com.ikemura.android_kotlin_lab
 
+import android.view.View
+import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
 
 // bindingという引数を受け取ってUnitを返す変数をtypealiasで宣言している
 typealias UserDataBindingBlock<Binding> = (binding: Binding) -> Unit
@@ -19,5 +23,16 @@ class UserBinding<Binding : ViewDataBinding>(
                 it.executePendingBindings()
             }
         }
+    }
+}
+
+class UserBindingHelper<T : ViewDataBinding> {
+    operator fun getValue(fragment: Fragment, prop: Any?): UserBinding<T> {
+        val binding = fragment.view?.setUpBinding(fragment.viewLifecycleOwner)
+        return UserBinding(binding)
+    }
+
+    private fun View.setUpBinding(lifecycleOwner: LifecycleOwner): T? {
+        return DataBindingUtil.bind<T>(this)?.also { it.lifecycleOwner = lifecycleOwner }
     }
 }
