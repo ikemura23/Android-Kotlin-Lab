033d77e24edb5599e3937106115cb01e220c285b
ikemura23 <banbara.work@gmail.com>
2020-09-13 00:02:26 +0900

    文字のBackgroundに色をつける（SpannableStringBuilder 版）

--- app/src/main/java/com/ikemura/android_kotlin_lab/extention/TextExt.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/extention/TextExt.kt
@@ -2,11 +2,15 @@ package com.ikemura.android_kotlin_lab.extention
 
 import android.text.SpannableStringBuilder
 import android.text.Spanned
+import android.text.style.BackgroundColorSpan
 import android.text.style.UnderlineSpan
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.core.text.HtmlCompat
+import androidx.core.text.color
 import androidx.core.text.underline
 import androidx.databinding.BindingAdapter
+import com.ikemura.android_kotlin_lab.R
 
 /**
  * TextViewに下線付きで文字を表示する（HtmlCompat 版）
@@ -36,4 +40,20 @@ fun TextView.textWithUnderLine2(text: String?) {
 fun TextView.textWithRedColor(text: String?) {
     if (text.isNullOrEmpty()) return
     this.text = HtmlCompat.fromHtml("ここは<font color='red'>$text</font>", HtmlCompat.FROM_HTML_MODE_COMPACT)
+}
+
+/**
+ * 文字のBackgroundに色をつける（SpannableStringBuilder 版）
+ */
+@BindingAdapter("textWithRedColor2")
+fun TextView.textWithRedColor2(text: String?) {
+    if (text.isNullOrEmpty()) return
+    this.text = SpannableStringBuilder().color(R.color.colorPrimary) {
+        append("ここは")
+        val start = length
+        append(text)
+        val end = length
+        val color = ContextCompat.getColor(this@textWithRedColor2.context, R.color.colorAccent)
+        setSpan(BackgroundColorSpan(color), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+    }
 }
