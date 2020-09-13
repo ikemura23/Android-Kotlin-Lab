f50139bc120d571b7b1158ea4657b43918af07b2
ikemura23 <banbara.work@gmail.com>
2020-09-13 22:46:03 +0900

    背景色をつける

--- app/src/main/java/com/ikemura/android_kotlin_lab/extention/TextExt.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/extention/TextExt.kt
@@ -4,6 +4,7 @@ import android.text.SpannableStringBuilder
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.text.HtmlCompat
+import androidx.core.text.backgroundColor
 import androidx.core.text.color
 import androidx.core.text.underline
 import androidx.databinding.BindingAdapter
@@ -39,11 +40,23 @@ fun TextView.textWithRedColor(text: String?) {
 }
 
 /**
- * 文字のBackgroundに色をつける（SpannableStringBuilder 版）
+ * TextViewに文字色をつける（SpannableStringBuilder 版）
  */
 @BindingAdapter("textWithRedColor2")
 fun TextView.textWithRedColor2(text: String?) {
     if (text.isNullOrEmpty()) return
     val color = ContextCompat.getColor(this@textWithRedColor2.context, R.color.colorAccent)
     this.text = SpannableStringBuilder().color(color) { append(text) }
+}
+
+/**
+ * 文字のBackgroundに色をつける（SpannableStringBuilder 版）
+ */
+@BindingAdapter("textWitBackgroundColor")
+fun TextView.textWitBackgroundColor(text: String?) {
+    if (text.isNullOrEmpty()) return
+    val color = ContextCompat.getColor(this.context, R.color.colorAccent)
+    this.text = SpannableStringBuilder().backgroundColor(color) {
+        append(text)
+    }
 }
