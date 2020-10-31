14dcb1ca416652b125edb7e89d907dec8a58f935
ikemura23 <banbara.work@gmail.com>
2020-10-31 23:41:58 +0900

    DataBinding化

--- app/src/main/java/com/ikemura/android_kotlin_lab/tab/PagerFragment.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/tab/PagerFragment.kt
@@ -1,7 +1,18 @@
 package com.ikemura.android_kotlin_lab.tab
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ikemura.android_kotlin_lab.R
+import com.ikemura.android_kotlin_lab.databinding.PagerFragmentBinding
 
 class PagerFragment : Fragment(R.layout.pager_fragment) {
+    private lateinit var binding: PagerFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = PagerFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
