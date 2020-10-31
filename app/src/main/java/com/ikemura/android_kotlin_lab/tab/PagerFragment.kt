c6b35231ee8b1e6941624b55cc754c6ef242b1ea
ikemura23 <banbara.work@gmail.com>
2020-10-31 23:37:53 +0900

    create PagerFragment

---
+++ app/src/main/java/com/ikemura/android_kotlin_lab/tab/PagerFragment.kt
@@ -0,0 +1,7 @@
+package com.ikemura.android_kotlin_lab.tab
+
+import androidx.fragment.app.Fragment
+import com.ikemura.android_kotlin_lab.R
+
+class PagerFragment : Fragment(R.layout.pager_fragment) {
+}
