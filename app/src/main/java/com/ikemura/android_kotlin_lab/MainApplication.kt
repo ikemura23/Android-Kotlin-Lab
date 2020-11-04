c86ae613252991b56e68eb504d0fd55102eb2e40
ikemura23 <banbara.work@gmail.com>
2020-11-04 11:45:12 +0900

    CameraXConfig.Provider は不要になったらしい

--- app/src/main/java/com/ikemura/android_kotlin_lab/MainApplication.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/MainApplication.kt
@@ -1,12 +1,12 @@
 package com.ikemura.android_kotlin_lab
 
 import android.app.Application
-import androidx.camera.camera2.Camera2Config
-import androidx.camera.core.CameraXConfig
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class MainApplication : Application(), CameraXConfig.Provider {
+class MainApplication : Application()
+// , CameraXConfig.Provider
+{
     override fun onCreate() {
         super.onCreate()
 
@@ -16,7 +16,7 @@ class MainApplication : Application(), CameraXConfig.Provider {
         }
     }
 
-    override fun getCameraXConfig(): CameraXConfig {
-        return Camera2Config.defaultConfig()
-    }
+    // override fun getCameraXConfig(): CameraXConfig {
+    //     return Camera2Config.defaultConfig()
+    // }
 }
