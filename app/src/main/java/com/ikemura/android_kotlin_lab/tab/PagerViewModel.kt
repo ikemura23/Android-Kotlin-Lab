393904fd59f4d32e479dd8901b409d57fdecf8fa
ikemura23 <banbara.work@gmail.com>
2020-11-14 13:39:26 +0900

    コンストラクタでDummyRepositoryを受け取る

--- app/src/main/java/com/ikemura/android_kotlin_lab/tab/PagerViewModel.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/tab/PagerViewModel.kt
@@ -1,6 +1,9 @@
 package com.ikemura.android_kotlin_lab.tab
 
 import androidx.lifecycle.ViewModel
+import com.ikemura.android_kotlin_lab.repository.DummyRepository
 
-class PagerViewModel : ViewModel() {
+class PagerViewModel(
+    private val dummyRepository: DummyRepository
+) : ViewModel() {
 }
