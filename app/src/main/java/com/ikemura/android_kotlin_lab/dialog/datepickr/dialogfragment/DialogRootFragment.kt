3284e7b3a15e19200a21586485fce4b0e408a760
ikemura23 <banbara.work@gmail.com>
2020-11-14 14:02:00 +0900

    FragmentにViewModelInjectorを使用してViewModelを渡してみる

--- app/src/main/java/com/ikemura/android_kotlin_lab/dialog/datepickr/dialogfragment/DialogRootFragment.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/dialog/datepickr/dialogfragment/DialogRootFragment.kt
@@ -10,6 +10,7 @@ import com.google.android.material.datepicker.MaterialDatePicker
 import com.ikemura.android_kotlin_lab.R
 import com.ikemura.android_kotlin_lab.bindings
 import com.ikemura.android_kotlin_lab.databinding.FragmentDialogRootBinding
+import com.ikemura.android_kotlin_lab.di.ViewModelInjector.dialogRootViewModel
 import com.ikemura.android_kotlin_lab.repository.DummyRepository
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -19,14 +20,13 @@ import java.util.Locale
  */
 class DialogRootFragment : Fragment() {
     private val useBinding by bindings<FragmentDialogRootBinding>()
-    private lateinit var dialogRootViewModel: DialogRootViewModel
+    private val viewModel: DialogRootViewModel = dialogRootViewModel()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val repository = DummyRepository()
-        dialogRootViewModel = DialogRootViewModel(repository)
-        println(dialogRootViewModel.getPageData())
+        println(viewModel.getPageData())
         return inflater.inflate(R.layout.fragment_dialog_root, container, false)
     }
 
