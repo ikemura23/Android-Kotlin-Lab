f3d463dbd2e4f58c8a7e1131ab48644c7e7d36dd
ikemura23 <banbara.work@gmail.com>
2020-12-17 15:47:06 +0900

    LifecycleObserverを継承させた

--- app/src/main/java/com/ikemura/android_kotlin_lab/MainApplication.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/MainApplication.kt
@@ -1,10 +1,12 @@
 package com.ikemura.android_kotlin_lab
 
 import android.app.Application
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ProcessLifecycleOwner
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class MainApplication : Application() {
+class MainApplication : Application(), LifecycleObserver {
     override fun onCreate() {
         super.onCreate()
 
@@ -12,5 +14,6 @@ class MainApplication : Application() {
             androidContext(this@MainApplication)
             modules(appModule)
         }
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
     }
 }
