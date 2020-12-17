dc5852673fd024556df6541a4b179d60cd6b7e3b
ikemura23 <banbara.work@gmail.com>
2020-12-17 16:08:39 +0900

    MainApplicationに@OnLifecycleEventでライフサイクルEventを実装してみた

--- app/src/main/java/com/ikemura/android_kotlin_lab/MainApplication.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/MainApplication.kt
@@ -1,7 +1,10 @@
 package com.ikemura.android_kotlin_lab
 
 import android.app.Application
+import android.util.Log
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.ProcessLifecycleOwner
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -14,6 +17,38 @@ class MainApplication : Application(), LifecycleObserver {
             androidContext(this@MainApplication)
             modules(appModule)
         }
+
         ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onAppCreate() {
+        Log.d("MainApplication", "onCreate")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onAppStart() {
+        Log.d("MainApplication", "onStart")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onAppResume() {
+        Log.d("MainApplication", "onResume")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onAppPause() {
+        Log.d("MainApplication", "onPause")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onAppStop() {
+        Log.d("MainApplication", "onStop")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onAppDestroy() {
+        // 呼ばれない
+        Log.d("MainApplication", "onDestroy")
     }
 }
