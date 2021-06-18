b8d92437d28a424ce38ae2c87d05eff6b3074e1d
ikemura23 <banbara.work@gmail.com>
2021-06-18 23:04:37 +0900

    LocationPopupFragment と LocationPopupViewModel を作成

---
+++ app/src/main/java/com/ikemura/android_kotlin_lab/locationpopup/LocationPopupFragment.kt
@@ -0,0 +1,9 @@
+package com.ikemura.android_kotlin_lab.locationpopup
+
+import androidx.fragment.app.Fragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class LocationPopupFragment : Fragment() {
+
+    private val viewModel: LocationPopupViewModel by viewModel()
+}
---
+++ app/src/main/java/com/ikemura/android_kotlin_lab/locationpopup/LocationPopupViewModel.kt
@@ -0,0 +1,6 @@
+package com.ikemura.android_kotlin_lab.locationpopup
+
+import androidx.lifecycle.ViewModel
+
+class LocationPopupViewModel : ViewModel() {
+}
