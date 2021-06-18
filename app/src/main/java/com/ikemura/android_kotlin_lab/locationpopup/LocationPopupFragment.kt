2b79672f105bf30be1318f3f846220bf4fa6b713
ikemura23 <banbara.work@gmail.com>
2021-06-18 23:48:29 +0900

    LocationPopupFragmentにボタン処理を追加

--- app/src/main/java/com/ikemura/android_kotlin_lab/locationpopup/LocationPopupFragment.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/locationpopup/LocationPopupFragment.kt
@@ -1,9 +1,31 @@
 package com.ikemura.android_kotlin_lab.locationpopup
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.ikemura.android_kotlin_lab.databinding.LocationPopupFragmentBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LocationPopupFragment : Fragment() {
 
+    lateinit var binding: LocationPopupFragmentBinding
     private val viewModel: LocationPopupViewModel by viewModel()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = LocationPopupFragmentBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.locationPopupStartButton.setOnClickListener {
+            startLocationPoopup()
+        }
+    }
+
+    private fun startLocationPoopup() {
+        // TODO メイン処理
+    }
 }
