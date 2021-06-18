ca3cebb8cb4fca216765d31428af7038ca324cf9
ikemura23 <banbara.work@gmail.com>
2021-06-19 00:46:44 +0900

    位置情報permissionリクエストする処理を追加

--- app/src/main/java/com/ikemura/android_kotlin_lab/locationpopup/LocationPopupFragment.kt
+++ app/src/main/java/com/ikemura/android_kotlin_lab/locationpopup/LocationPopupFragment.kt
@@ -1,9 +1,12 @@
 package com.ikemura.android_kotlin_lab.locationpopup
 
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.ikemura.android_kotlin_lab.databinding.LocationPopupFragmentBinding
 import com.ikemura.android_kotlin_lab.locationpopup.dialog.LocationPopupDialogFragment
@@ -13,6 +16,14 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
  * 位置情報の説明dialog　-> 位置情報permission要求 の表示Fragment
  */
 class LocationPopupFragment : Fragment() {
+
+    // 単体
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            if (result) {
+                Log.d(TAG, "RequestPermission: $result")
+            }
+        }
 
     lateinit var binding: LocationPopupFragmentBinding
     private val viewModel: LocationPopupViewModel by viewModel()
@@ -24,12 +35,21 @@ class LocationPopupFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.locationPopupStartButton.setOnClickListener {
-            startLocationPopup()
-        }
+        startLocationPopup()
+        // binding.locationPopupStartButton.setOnClickListener {
+        //     startLocationPopup()
+        // }
     }
 
+    /**
+     * メイン処理スタート
+     */
     private fun startLocationPopup() {
         LocationPopupDialogFragment().show(childFragmentManager, LocationPopupDialogFragment::class.java.name)
+        requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    companion object {
+        private val TAG = LocationPopupFragment::class.java.name
     }
 }
